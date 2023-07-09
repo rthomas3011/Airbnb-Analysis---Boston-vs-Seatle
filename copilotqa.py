38a3d0a332498ee455b81d93820383a20dfb49ef
rthomas3011 <rijuthomas016@gmail.com>
2023-07-09 07:01:49 -0400

    changes to medium post

---
+++ copilotqa.py
@@ -0,0 +1,5 @@
+// q: what does SOLID stand for in OO programing?
+// a: Single Responsibility, Open/Closed, Liskov Substitution, Interface Segregation, Dependency Inversion
+
+// q: what is the difference between a class and an object?
+// a:
